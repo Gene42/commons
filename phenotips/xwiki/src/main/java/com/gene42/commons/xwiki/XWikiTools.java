1eb5441e378b129bddaa85f6cd5610c530510054
Alina Gvozdik <alina.gvo@gmail.com>
2018-08-17 17:22:30 -0400

    CLO-81: Add getXObjects from XWikiContext and EntityReferences method

--- phenotips/xwiki/src/main/java/com/gene42/commons/xwiki/XWikiTools.java
+++ phenotips/xwiki/src/main/java/com/gene42/commons/xwiki/XWikiTools.java
@@ -20,11 +20,14 @@ import org.xwiki.users.UserManager;
 import org.xwiki.velocity.tools.EscapeTool;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Provider;
@@ -54,6 +57,10 @@ public final class XWikiTools
     private static final int MAX_NUM_OF_GROUPS = 1000;
 
     private static final String ADMINISTRATORS_SUFFIX = " Administrators";
+
+    private static final String XWIKI = "XWiki";
+
+    private static final String XWIKI_PREFS = "XWikiPreferences";
 
     @Inject
     private UserManager userManager;
@@ -189,5 +196,27 @@ public final class XWikiTools
         }
 
         return result;
+    }
+
+    /**
+     * Allows to retrieve to retrieve a list of {@link BaseObject} given the XWiki {@code context} and
+     * {@code objectType}.
+     *
+     * @param context the {@link XWikiContext}
+     * @param objectType the {@link EntityReference}
+     * @return a list of objects of type {@code objectType}
+     */
+    @Nonnull
+    public static List<BaseObject> getXObjects(@Nonnull final XWikiContext context,
+        @Nullable final EntityReference objectType)
+    {
+        final XWiki xWiki = context.getWiki();
+        try {
+            final DocumentReference ref = new DocumentReference(xWiki.getDatabase(), XWIKI, XWIKI_PREFS);
+            final XWikiDocument prefsDoc = xWiki.getDocument(ref, context);
+            return XWikiTools.getXObjects(prefsDoc, objectType);
+        } catch (final XWikiException e) {
+            return Collections.emptyList();
+        }
     }
 }
